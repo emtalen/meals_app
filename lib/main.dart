a703c1e63b4c4b8525a30c2f58cb76d6048dae77
emtalen <59577366+emtalen@users.noreply.github.com>
2020-09-15 19:58:15 +0200

    changing the navigation system to a better one that is easier to use in bigger apps and therefor better practise

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_meals_screen.dart';
 import 'categories_screen.dart';
+import './category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +30,9 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {
+        '/category-meals': (ctx) =>  CategoryMealsScreen(),
+      },
     );
   }
 }
