ab95a9758ffcb3059c384319b817a1e6e420f485
emtalen <59577366+emtalen@users.noreply.github.com>
2020-09-20 13:32:41 +0200

    adding title to each meal

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -41,6 +41,27 @@ class MealItem extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
+                  Positioned(
+                    bottom: 20,
+                    right: 0,
+                    child: Container(
+                      width: 300,
+                      color: Colors.black54,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 20,
+                      ),
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                        ),
+                        softWrap: true,
+                        overflow: TextOverflow.fade,
+                      ),
+                    ),
+                  )
                 ],
               ),
             ],
