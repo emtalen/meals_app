cdff3022827bff9efaaddcea8adf200faef0f5ac
emtalen <59577366+emtalen@users.noreply.github.com>
2020-10-16 14:40:47 +0200

    adding a drawer

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buidlListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 24,
+            fontWeight: FontWeight.bold),
+      ),
+      onTap: () {
+        
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            color: Theme.of(context).accentColor,
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buidlListTile('Meals', Icons.restaurant),
+          buidlListTile('Filters', Icons.settings)
+        ],
+      ),
+    );
+  }
+}
